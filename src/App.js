799f71f05f72946e507705f7f2682a6934ab3ee3
fejul <juliafeller420@gmail.com>
2022-09-14 18:38:24 +0200

    preloader added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import DotLoader from 'react-spinners/DotLoader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ loading, setLoading ] = useState(false);
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 1800);
+	}, []);
+
+	return (
+		<React.Fragment>
+			{loading ? (
+				<div className="preloader">
+					<DotLoader color={'#FFEC3C'} loading={loading} size={120} />
+				</div>
+			) : (
+				<Navbar />
+			)}
+		</React.Fragment>
+	);
 }
 
 export default App;
+
+//useState -> to keep track of a loading variable that is going to tell us to display and remove the loading screen.
+//useEffect -> in this case it serves as our live cycle method. By adding an empty array it runs only one time when the page is rendered and when its rendered, we set the loading to true and after 5sek we set the loading to false. To that, we can use setTimeOut.
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import classes from './Navbar.module.css';
+const Navbar = () => {
+	return (
+		<nav className={classes.navbar}>
+			<a>JULIA FELLER</a>
+		</nav>
+	);
+};
+
+export default Navbar;
